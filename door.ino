79a8cb6baf2817d01610c4e72f6ab8b4ac2c4252
equlibrino <69399726+equlibrino@users.noreply.github.com>
2024-04-11 20:55:58 +0300

    folder

--- door.ino
+++ door.ino
@@ -1,26 +1,30 @@
+#ifdef ENABLE_DEBUG
+   #define DEBUG_ESP_PORT Serial
+   #define NODEBUG_WEBSOCKETS
+   #define NDEBUG
+#endif 
+
 #include <Arduino.h>
 #if defined(ESP8266)
-#include <ESP8266WiFi.h>
+  #include <ESP8266WiFi.h>
 #elif defined(ESP32) || defined(ARDUINO_ARCH_RP2040)
-#include <WiFi.h>
+  #include <WiFi.h>
 #endif
 
 #include "SinricPro.h"
 #include "SinricProSwitch.h"
 
-#define WIFI_SSID         "null"  
-#define WIFI_PASS         "null" 
-#define APP_KEY           "null"  
-#define APP_SECRET        "null" 
-#define SWITCH_ID_1       "null" // 2 Swıtch
+#define WIFI_SSID         "your_wifi_ssid"
+#define WIFI_PASS         "your_wifi_password"
+#define APP_KEY           "your_sinricpro_app_key"
+#define APP_SECRET        "your_sinricpro_app_secret"
 
-#if defined(ESP8266)
-#define RELAYPIN_1        5
-#define RELAYPIN_2        4
-#endif
+#define SWITCH_ID_1       "your_switch_id" 
+#define RELAYPIN_1        2 // GPIO 2 for ESP8266
+#define RELAYPIN_2        0 // GPIO 0 for ESP8266
 
 #define BAUD_RATE         115200 // Change baudrate to your need
-                      
+
 bool onPowerState(const String &deviceId, bool &state) {
   Serial.printf("Device turned %s", state ? "on" : "off");
   digitalWrite(RELAYPIN_1, state ? LOW : HIGH);
@@ -37,18 +41,17 @@ bool onPowerState(const String &deviceId, bool &state) {
   return true; // request handled properly
 
 }
-
 // setup function for WiFi connection
 void setupWiFi() {
   Serial.printf("\r\n[Wifi]: Connecting");
 
-#if defined(ESP8266)
-  WiFi.setSleepMode(WIFI_NONE_SLEEP);
-  WiFi.setAutoReconnect(true);
-#elif defined(ESP32)
-  WiFi.setSleep(false);
-  WiFi.setAutoReconnect(true);
-#endif
+  #if defined(ESP8266)
+    WiFi.setSleepMode(WIFI_NONE_SLEEP); 
+    WiFi.setAutoReconnect(true);
+  #elif defined(ESP32)
+    WiFi.setSleep(false); 
+    WiFi.setAutoReconnect(true);
+  #endif
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
@@ -63,20 +66,16 @@ void setupWiFi() {
 // setup function for SinricPro
 void setupSinricPro() {
   // add devices and callbacks to SinricPro
-  pinMode(RELAYPIN_1, OUTPUT); 
-  pinMode(RELAYPIN_2, OUTPUT);
-
+  pinMode(RELAYPIN_1, OUTPUT);
+  pinMode(RELAYPIN_2, OUTPUT); 
   SinricProSwitch& mySwitch = SinricPro[SWITCH_ID_1];
   mySwitch.onPowerState(onPowerState);
   
+  
   // setup SinricPro
-  SinricPro.onConnected([]() {
-    Serial.printf("Connected to SinricPro\r\n");
-  });
-  SinricPro.onDisconnected([]() {
-    Serial.printf("Disconnected from SinricPro\r\n");
-  });
-
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+   
   SinricPro.begin(APP_KEY, APP_SECRET);
 }
 
@@ -92,3 +91,4 @@ void setup() {
 void loop() {
   SinricPro.handle();
 }
+//Equlibrino - Esp01-S kartı için hazırlanmıştır.
